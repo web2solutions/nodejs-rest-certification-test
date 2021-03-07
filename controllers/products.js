9ab47dcdb49696202c7576891a9679cf3355aa9f
Eduardo Almeida <web2solucoes@gmail.com>
2021-03-06 23:46:52 -0300

    fix

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,11 @@
-const Products = require('../models/products');
+const Products = require('../models/products')
 
 exports.list = async function (req, res) {
-  console.log('xcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcxc')
-  const products = await Products.findAll();
-  console.log('=============================>',products)
+  const products = await Products.findAll()
   return res.json(products)
-};
+}
 
 exports.post = async function (req, res) {
-  //console.log(req.body)
   const record = await Products.create({
     name: req.body.name,
     price: req.body.price,
@@ -17,31 +14,21 @@ exports.post = async function (req, res) {
     isPublished: false
   })
   return res.status(201).json(record)
-  res.send('NOT IMPLEMENTED: Author list');
-};
+}
 
 
 exports.patch = function (req, res) {
-  (async function () {
-    //console.log('>>>>>>>>>>>', req.body)
+  ;(async function () {
     const id = req.params.id
-    //console.log('>>>>>>>>>>>', req.params.id)
-    const product = await Products.findByPk(id);
-    
-    let failed = false
-    const errors = []
+    const product = await Products.findByPk(id)
     if (product) {
       if (product.mrp >= product.price) {
         if (product.stock > 0) {
-          console.log(product)
           product.isPublished = true
           const r = await product.save()
-          // console.log('====================>', r)
-          console.log(product)
           return res.status(204).send()
         }
         else {
-          failed = true
           return res.status(422).json(['Stock count is 0'])
         }
       }
@@ -52,13 +39,10 @@ exports.patch = function (req, res) {
         return res.status(422).json(['MRP should be less than equal to the Price'])
       }
     }
-    
-
-
     return res.status(204)
   })()
-};
+}
 
 exports.delete = async function (req, res) {
-  res.status(405).send('NOT ALLOWED');
-};
+  res.status(405).send('NOT ALLOWED')
+}
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const controller = require('../controllers/products');
+const router = require('express').Router()
+const controller = require('../controllers/products')
 
 
-router.get('/products', controller.list);
-router.post('/products', controller.post);
-router.patch('/products/:id', controller.patch);
-router.put('/products/:id', controller.delete);
-router.delete('/products/:id', controller.delete);
+router.get('/products', controller.list)
+router.post('/products', controller.post)
+router.patch('/products/:id', controller.patch)
+router.put('/products/:id', controller.delete)
+router.delete('/products/:id', controller.delete)
 
-module.exports = router;
+module.exports = router
